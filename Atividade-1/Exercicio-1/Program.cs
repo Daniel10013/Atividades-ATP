cdfae2d3711ecbe8024dffd775d2208f94bc7986
Daniel Filipe Soares Corrêa Vilaça <1526493@sga.pucminas.br>
2024-03-04 22:03:26 -0300

    Finalizar - primeiro exercicio

--- Atividade-1/Exercicio-1/Program.cs
+++ Atividade-1/Exercicio-1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program{
+    public static void Main(){
+        int figureBase, figureHeight, figurePerimeter, figureArea;
+        double figureDiagonal;
+        Console.WriteLine("Ativiade 1 - Exercício 1 \n");
+        //entrada de dados
+        Console.WriteLine("Digite o valor da base do retângulo:");
+        figureBase = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o valor da altura do retângulo:");
+        figureHeight = int.Parse(Console.ReadLine());
+
+        //procesamento de dados
+        figurePerimeter = (figureBase +  figureHeight) * 2;
+        figureArea = figureBase * figureHeight;
+        figureDiagonal = Math.Sqrt(Math.Pow(figureBase, 2) + Math.Pow(figureArea, 2));
+
+        //saida de dados
+        Console.WriteLine("O valor do perimetro do retângulo é: "+ figurePerimeter);
+        Console.WriteLine("O valor da área do retângulo é: "+ figureArea);
+        Console.WriteLine($"O valor da diagonal do retângulo é: {0:2F}", figureDiagonal);
+    }
+}
+
