9837d5f176d03cceeb007542bc0d813c269bad09
Daniel Filipe Soares Corrêa Vilaça <1526493@sga.pucminas.br>
2024-03-22 20:52:50 -0300

    Finalizando a atividade 7

---
+++ Lista-1/Exercicio-7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Ativiade 1 - Exercício 7 \n");
+    
+        int daysWithoutAciddents, daysSubtracted, months, years, days;
+        string messageDays, messageMonths, messageYears;
+        //entrada de dados
+        Console.WriteLine("Insiria a quantidade de dias sem acidentes: ");
+        daysWithoutAciddents = int.Parse(Console.ReadLine());
+
+        //processamento de dados
+        years = daysWithoutAciddents / 365;
+        daysSubtracted = daysWithoutAciddents % 365;
+
+        months = daysSubtracted / 30;
+        daysSubtracted = daysSubtracted % 30;
+
+        days = daysSubtracted;
+
+        //saide de dados
+        Console.WriteLine(daysWithoutAciddents + " dias transformado para anos, meses e dias é: ");
+        Console.WriteLine($"Anos: {years}, Meses: {months}, Dias: {days}");
+    }
+}
+
